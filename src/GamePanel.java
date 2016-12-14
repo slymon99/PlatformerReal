cc869aeee9461beba1a55822a80539c0e56bb68d
Simon <simonclark236@gmail.com>
2016-12-14 17:15:46 -0500

    played with mass of player

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -70,11 +70,92 @@ public class GamePanel extends JPanel implements KeyListener{
                 new Vector2(0.5, -0.5));
         GameObject triangle = new GameObject();
         triangle.addFixture(triShape);
-        triangle.setMass(MassType.NORMAL);
+        Mass m = new Mass(new Vector2(0,0),20.0,20.0);
+        triangle.setMass(m);
         triangle.translate(-1.0, 2.0);
         // test having a velocity
         triangle.getLinearVelocity().set(5.0, 0.0);
         this.world.addBody(triangle);
+
+        // create a circle
+        Circle cirShape = new Circle(0.5);
+        GameObject circle = new GameObject();
+        circle.addFixture(cirShape);
+        circle.setMass(MassType.NORMAL);
+        circle.translate(2.0, 2.0);
+        // test adding some force
+        circle.applyForce(new Vector2(-100.0, 0.0));
+        // set some linear damping to simulate rolling friction
+        circle.setLinearDamping(0.05);
+        this.world.addBody(circle);
+
+        // try a rectangle
+        Rectangle rectShape = new Rectangle(1.0, 1.0);
+        GameObject rectangle = new GameObject();
+        rectangle.addFixture(rectShape);
+        rectangle.setMass(MassType.NORMAL);
+        rectangle.translate(0.0, 2.0);
+        rectangle.getLinearVelocity().set(-5.0, 0.0);
+        this.world.addBody(rectangle);
+
+        // try a polygon with lots of vertices
+        Polygon polyShape = Geometry.createUnitCirclePolygon(10, 1.0);
+        GameObject polygon = new GameObject();
+        polygon.addFixture(polyShape);
+        polygon.setMass(MassType.NORMAL);
+        polygon.translate(-2.5, 2.0);
+        // set the angular velocity
+        polygon.setAngularVelocity(Math.toRadians(-20.0));
+        this.world.addBody(polygon);
+
+        // try a compound object
+        Circle c1 = new Circle(0.5);
+        BodyFixture c1Fixture = new BodyFixture(c1);
+        c1Fixture.setDensity(0.5);
+        Circle c2 = new Circle(0.5);
+        BodyFixture c2Fixture = new BodyFixture(c2);
+        c2Fixture.setDensity(0.5);
+        Rectangle rm = new Rectangle(2.0, 1.0);
+        // translate the circles in local coordinates
+        c1.translate(-1.0, 0.0);
+        c2.translate(1.0, 0.0);
+        GameObject capsule = new GameObject();
+        capsule.addFixture(c1Fixture);
+        capsule.addFixture(c2Fixture);
+        capsule.addFixture(rm);
+        capsule.setMass(MassType.NORMAL);
+        capsule.translate(0.0, 4.0);
+        this.world.addBody(capsule);
+
+        GameObject issTri = new GameObject();
+        issTri.addFixture(Geometry.createIsoscelesTriangle(1.0, 3.0));
+        issTri.setMass(MassType.NORMAL);
+        issTri.translate(2.0, 3.0);
+        this.world.addBody(issTri);
+
+        GameObject equTri = new GameObject();
+        equTri.addFixture(Geometry.createEquilateralTriangle(2.0));
+        equTri.setMass(MassType.NORMAL);
+        equTri.translate(3.0, 3.0);
+        this.world.addBody(equTri);
+
+        GameObject rightTri = new GameObject();
+        rightTri.addFixture(Geometry.createRightTriangle(2.0, 1.0));
+        rightTri.setMass(MassType.NORMAL);
+        rightTri.translate(4.0, 3.0);
+        this.world.addBody(rightTri);
+
+        GameObject cap = new GameObject();
+        cap.addFixture(new Capsule(1.0, 0.5));
+        cap.setMass(MassType.NORMAL);
+        cap.translate(-3.0, 3.0);
+        this.world.addBody(cap);
+
+        GameObject slice = new GameObject();
+        slice.addFixture(new Slice(0.5, Math.toRadians(120)));
+        slice.setMass(MassType.NORMAL);
+        slice.translate(-3.0, 3.0);
+        this.world.addBody(slice);
     }
 
     public void start() {
@@ -134,13 +215,13 @@ public class GamePanel extends JPanel implements KeyListener{
 
     private void passKeys(){
         if(isKeyPressed(KeyEvent.VK_A)){
-            world.getBody(1).applyForce(new Force(-.25,0));
+            world.getBody(1).applyForce(new Force(-10,0));
         }
         if(isKeyPressed(KeyEvent.VK_D)){
-            world.getBody(1).applyForce(new Force(.25,0));
+            world.getBody(1).applyForce(new Force(10,0));
         }
         if(isKeyPressed(KeyEvent.VK_W)){
-            world.getBody(1).applyForce(new Force(0,.8));
+            world.getBody(1).applyForce(new Force(0,40));
         }
     }
 
