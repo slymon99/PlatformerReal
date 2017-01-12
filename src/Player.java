24f6c09cc41886dd144ffcecdb84ca6a31234741
simon_clark <simonclark236@gmail.com>
2017-01-12 14:11:31 -0500

    deleted depreciated jump detection function

--- src/Player.java
+++ src/Player.java
@@ -17,22 +17,6 @@ public class Player extends GameObject {
         ticksSinceJumped = 0;
     }
 
-    public void detectLandedness(List<Body> bodies) {
-
-
-        for (Body b : bodies) {
-            if (!b.equals(this)) {
-//                System.out.println(b.getFixture(b.getWorldCenter()).getShape());
-//                System.out.println(getWorldCenter().getYComponent().subtract(b.getWorldCenter().getYComponent()));
-//                System.out.println(b.getWorldCenter().getYComponent().getMagnitude());
-
-            }
-
-
-        }
-
-    }
-
     public Ray getJumpDetectionRay() {
         return (new Ray(getWorldCenter(), 3 * Math.PI / 2));
     }
