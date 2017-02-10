ef20365a9312561b0b8e43dda12132565e40040d
simon_clark <simonclark236@gmail.com>
2017-02-10 12:29:46 -0500

    dragging boxes in leveleditor

--- src/LevelEditor.java
+++ src/LevelEditor.java
@@ -3,21 +3,26 @@ import org.dyn4j.geometry.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Rectangle;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 
 /**
  * Created by simon_clark on 1/23/17.
  */
-public class LevelEditor extends JPanel  implements MouseListener{
+public class LevelEditor extends JPanel  implements MouseListener, KeyListener, MouseMotionListener{
     private int rectX, rectY, width, height;
     private ArrayList<Rectangle> myRects;
+    private boolean[] keys;
+    private Rectangle outline;
 
 
     public LevelEditor(){
         addMouseListener(this);
+        addMouseMotionListener(this);
+        addKeyListener(this);
+        keys = new boolean[1000];
+        outline = new Rectangle();
         myRects = new ArrayList<Rectangle>();
 
         myRects.add(new Rectangle(200,300, 40, 40));
@@ -31,6 +36,7 @@ public class LevelEditor extends JPanel  implements MouseListener{
         window.setBounds(0, 0, 1440, 850);
         LevelEditor panel = new LevelEditor();
         panel.grabFocus();
+        panel.setFocusable(true);
         window.add(panel);
         window.setVisible(true);
 
@@ -64,14 +70,12 @@ public class LevelEditor extends JPanel  implements MouseListener{
         rectX=e.getX()-720;
         rectY=-e.getY()+425;
 
-        System.out.println(e.getX() + " " + rectX);
-
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         width=e.getX()-rectX-720;
-        height= -(e.getY()-rectY)+425;
+        height= -e.getY()+425 - rectY;
         myRects.add(new Rectangle(rectX, rectY, width, height));
         repaint();
 
@@ -86,5 +90,42 @@ public class LevelEditor extends JPanel  implements MouseListener{
     @Override
     public void mouseExited(MouseEvent e) {
 
+    }
+
+    public boolean isKeyPressed(int code) {
+        return keys[code];
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        keys[keyEvent.getKeyCode()] = true;
+        repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+        keys[keyEvent.getKeyCode()] = false;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        width=e.getX()-rectX-720;
+        height= -e.getY()+425 - rectY;
+        outline.setBounds(rectX, rectY, width, height);
+        repaint();
+        System.out.println(round(2) + " " + round(8) + " " + round(18));
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    private int round(int n){
+        return Math.round(n/10) * 10;
     }
 }
