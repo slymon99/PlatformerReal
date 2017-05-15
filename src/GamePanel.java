53ee03fec5932e51d5abac43f5abc7ca8b0d16eb
simon_clark <simonclark236@gmail.com>
2017-05-15 13:24:47 -0400

    cleaning

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -25,7 +25,7 @@ import org.dyn4j.geometry.Vector2;
 /**
  * Created by Simon on 12/3/2016.
  */
-public class GamePanel extends JPanel implements KeyListener{
+public class GamePanel extends JPanel implements KeyListener {
 
     private boolean stopped;
     private GameWorld world;
@@ -61,7 +61,7 @@ public class GamePanel extends JPanel implements KeyListener{
         // setup the world
         this.initializeWorld(currentLevel);
 
-        Timer graphicsUpdate = new Timer(1000/10, (ActionEvent e) -> {
+        Timer graphicsUpdate = new Timer(1000 / 10, (ActionEvent e) -> {
             repaint();
         });
 
@@ -90,18 +90,6 @@ public class GamePanel extends JPanel implements KeyListener{
         lanky.setMassType(MassType.FIXED_ANGULAR_VELOCITY);
         world.addLanky(lanky);
 
-
-//        Rectangle pusher = new Rectangle(5,1);
-//        GameObject puusher = new GameObject();
-//        puusher.addFixture(pusher,.01);
-//        puusher.setMass(MassType.NORMAL);
-//        puusher.translate(-19.5,3.9);
-//        world.addBody(puusher);
-////
-//
-//
-
-//
         //loads first level
         System.out.println("preparing array from levelController");
         Level l = lc.readLevel(levelNum);
@@ -118,94 +106,6 @@ public class GamePanel extends JPanel implements KeyListener{
         Goal goal = new Goal(l.getGoalPoint().getX(), l.getGoalPoint().getY());
         world.addBody(goal);
 
-//        Lava testLava = new Lava(-10, -2, 7, 5);
-//        world.addBody(testLava);
-
-
-//        world.raycast()
-
-//        // create a circle
-//        Circle cirShape = new Circle(0.5);
-//        GameObject circle = new GameObject();
-//        circle.addFixture(cirShape);
-//        circle.setMass(MassType.NORMAL);
-//        circle.translate(2.0, 2.0);
-//        // test adding some force
-//        circle.applyForce(new Vector2(-100.0, 0.0));
-//        // set some linear damping to simulate rolling friction
-//        circle.setLinearDamping(0.05);
-//        this.world.addBody(circle);
-//
-//        // try a rectangle
-//        for(int i = 0; i<100; i++) {
-//            Circle rectShape = new Circle(1.0);
-//            GameObject rectangle = new GameObject();
-//            rectangle.addFixture(rectShape, 0.5,.2, .3);
-//            rectangle.setMass(MassType.NORMAL);
-//            rectangle.translate(0, 10.0);
-//
-//            this.world.addBody(rectangle);
-//        }
-//
-//        // try a polygon with lots of vertices
-//        Polygon polyShape = Geometry.createUnitCirclePolygon(10, 1.0);
-//        GameObject polygon = new GameObject();
-//        polygon.addFixture(polyShape);
-//        polygon.setMass(MassType.NORMAL);
-//        polygon.translate(-2.5, 2.0);
-//        // set the angular velocity
-//        polygon.setAngularVelocity(Math.toRadians(-20.0));
-//        this.world.addBody(polygon);
-
-        // try a compound object
-//        Circle c1 = new Circle(0.5);
-//        BodyFixture c1Fixture = new BodyFixture(c1);
-//        c1Fixture.setDensity(0.5);
-//        Circle c2 = new Circle(0.5);
-//        BodyFixture c2Fixture = new BodyFixture(c2);
-//        c2Fixture.setDensity(0.5);
-//        Rectangle rm = new Rectangle(2.0, 1.0);
-//        // translate the circles in local coordinates
-//        c1.translate(-1.0, 0.0);
-//        c2.translate(1.0, 0.0);
-//        GameObject capsule = new GameObject();
-//        capsule.addFixture(c1Fixture);
-//        capsule.addFixture(c2Fixture);
-//        capsule.addFixture(rm);
-//        capsule.setMass(MassType.NORMAL);
-//        capsule.translate(0.0, 4.0);
-//        this.world.addBody(capsule);
-
-
-//        GameObject issTri = new GameObject();
-//        issTri.addFixture(Geometry.createIsoscelesTriangle(1.0, 3.0));
-//        issTri.setMass(MassType.NORMAL);
-//        issTri.translate(2.0, 3.0);
-//        this.world.addBody(issTri);
-//
-//        GameObject equTri = new GameObject();
-//        equTri.addFixture(Geometry.createEquilateralTriangle(2.0));
-//        equTri.setMass(MassType.NORMAL);
-//        equTri.translate(3.0, 3.0);
-//        this.world.addBody(equTri);
-//
-//        GameObject rightTri = new GameObject();
-//        rightTri.addFixture(Geometry.createRightTriangle(2.0, 1.0));
-//        rightTri.setMass(MassType.NORMAL);
-//        rightTri.translate(4.0, 3.0);
-//        this.world.addBody(rightTri);
-//
-//        GameObject cap = new GameObject();
-//        cap.addFixture(new Capsule(1.0, 0.5));
-//        cap.setMass(MassType.NORMAL);
-//        cap.translate(-3.0, 3.0);
-//        this.world.addBody(cap);
-//
-//        GameObject slice = new GameObject();
-//        slice.addFixture(new Slice(0.5, Math.toRadians(120)));
-//        slice.setMass(MassType.NORMAL);
-//        slice.translate(-3.0, 3.0);
-//        this.world.addBody(slice);
     }
 
     public void start() {
@@ -250,7 +150,7 @@ public class GamePanel extends JPanel implements KeyListener{
         this.last = time;
         // convert from nanoseconds to seconds
         double elapsedTime = diff / NANO_TO_BASE;
-        System.out.println("tps: " + 1/elapsedTime);
+        System.out.println("tps: " + 1 / elapsedTime);
         // update the worldDepreciated with the elapsed time and current keys pressed
 
         passKeys();
@@ -322,7 +222,7 @@ public class GamePanel extends JPanel implements KeyListener{
         squat.tickUp();
         lanky.tickUp();
 
-        if(isKeyPressed(KeyEvent.VK_R)){
+        if (isKeyPressed(KeyEvent.VK_R)) {
             initializeWorld(currentLevel);
         }
 
@@ -387,19 +287,30 @@ public class GamePanel extends JPanel implements KeyListener{
 
     private void levelSettings(int level) {
         if (level == 0) {
-            Rectangle pusher = new Rectangle(5,1);
-            GameObject puusher = new GameObject();
-            puusher.addFixture(pusher,.01);
-            puusher.setMass(MassType.NORMAL);
-            puusher.translate(-19.5,3.9);
-            world.addBody(puusher);
+//            Rectangle pusher = new Rectangle(5,1);
+//            GameObject puusher = new GameObject();
+//            puusher.addFixture(pusher,.01);
+//            puusher.setMass(MassType.NORMAL);
+//            puusher.translate(-19.5,3.9);
+//            world.addBody(puusher);
+//
+//            Circle pushed = new Circle(2);
+//            GameObject puushed = new GameObject();
+//            puushed.addFixture(pushed, .1,.1,0.5);
+//            puushed.setMass(MassType.NORMAL);
+//            puushed.translate(-15,4.5);
+//            world.addBody(puushed);
 
-            Circle pushed = new Circle(2);
-            GameObject puushed = new GameObject();
-            puushed.addFixture(pushed, .1,.1,0.5);
-            puushed.setMass(MassType.NORMAL);
-            puushed.translate(-15,4.5);
-            world.addBody(puushed);
+            for (int i = 0; i < 10; i++) {
+                Circle circle = new Circle(2);
+                GameObject circleObject = new GameObject();
+                circleObject.addFixture(circle, .1, .1, 1);
+                circleObject.setMass(new Mass(new Vector2(0,0), 20, 10));
+                circleObject.translate(-30+6*i, 10);
+                circleObject.setColor(new Color(62, 62, 62));
+                circleObject.applyForce(new Force(100,0));
+                world.addBody(circleObject);
+            }
 
         } else if (level == 1) {
 
